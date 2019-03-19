07853b6d532f8f71d6c8c56ed3a07073b35a43a9
Léonard Marques <lmarques@student.42.fr>
2019-03-19 19:37:12 +0100

    updated main

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import System.Environment
-
-import Parser.Parser
+import Parser
 
 main :: IO ()
 main = do
     args <- getArgs 
     content <- readFile (args !! 0)
     let l = lines content
-    mapM_ putStrLn l
+    let w = map (\l' -> words l') $ clearInput $ drop 1 l
+    case transformInput w of
+        Just w' -> do
+            mapM_ (\z-> putStrLn (show z)) $ concat w'
+        Nothing -> putStrLn "KO"
