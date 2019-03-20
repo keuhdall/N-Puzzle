77665ad49ddc148bdc14c5c5b607c6a98481be22
Léonard Marques <lmarques@student.42.fr>
2019-03-20 01:31:40 +0100

    finised parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,6 @@
 module Parser where
     import Data.Char
+    import Data.Sort
     import Data.List.Split
 
     maxPuzzle :: Int
@@ -9,10 +10,16 @@ module Parser where
     clearInput :: [String] -> [String]
     clearInput xs = filter (/="") $ map (\x -> (splitOn "#" x) !! 0) xs
 
+    -- Checking size of the puzzle
     isValidSize :: [[Int]] -> Bool
     isValidSize xss = let ys = map (\y -> y^2) [3..maxPuzzle] in length (concat xss) `elem` ys
 
+    -- Sorts input and compare it to an enum list of the same size
+    hasValidContent :: [[Int]] -> Bool
+    hasValidContent xss = let n = (length xs) - 1; xs = (concat xss); ys = [0..n] in (sort xs) == ys
+
+    -- Returns [[Int]] if the input is valid, otherwise returns Nothing
     transformInput :: [[String]] -> Maybe [[Int]]
     transformInput xs
         | all isDigit (concat (concat xs)) == False = Nothing
-        | otherwise = let xss = map (\x -> map (\y -> read y) x) xs in if isValidSize xss then Just (xss) else Nothing
+        | otherwise = let xss = map (\x -> map (\y -> read y) x) xs in if (isValidSize xss && hasValidContent xss)  then Just (xss) else Nothing
