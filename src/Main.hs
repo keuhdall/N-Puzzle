a9ae3f0d8aabf8dbdfaaa1422b7a09ac1bff1af7
keuhdall <lmarques@student.42.fr>
2019-04-02 18:11:05 +0200

    fixed priority error while parsing input

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,7 @@ main = do
     args <- getArgs 
     content <- readFile (args !! 0)
     let l = lines content
-    let w = map (\l' -> words l') $ clearInput $ drop 1 l
+    let w = map (\l' -> words l') $ drop 1 $ clearInput l
     case transformInput w of
         Just w' -> do
             if isSolvable (concat w') then mapM_ (\z-> putStrLn $ show z) $ concat w' else putStrLn "NOP."
