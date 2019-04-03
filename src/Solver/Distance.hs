a049c4357e9d6bece5ccfe7dfe78a8bad2735a2a
Leonard Marques <keuhdall@gmail.com>
2019-04-03 03:48:06 +0200

    Added Hamming distance ; changed back distance function signatures

--- src/Solver/Distance.hs
+++ src/Solver/Distance.hs
@@ -1,9 +1,14 @@
 module Solver.Distance where
-    manhattanDistance :: (Int, Int) -> (Int, Int) -> Float
-    manhattanDistance x y = fromIntegral $ abs (fst x - fst y) + abs (snd x - snd y)
+    import Data.Bits
 
-    diagonalDistance :: (Int, Int) -> (Int, Int) -> Float
-    diagonalDistance x y = let x' = (abs $ fst x - fst y); y' = (abs $ snd x - snd y) in fromIntegral $ max x' y'
+    manhattanDistance :: (Int, Int) -> (Int, Int) -> Int
+    manhattanDistance x y = abs (fst x - fst y) + abs (snd x - snd y)
 
-    euclidianDistance :: (Int, Int) -> (Int, Int) -> Float
-    euclidianDistance x y = sqrt $ fromIntegral $ (fst x - fst y)*2 + (snd x - snd y)*2
+    diagonalDistance :: (Int, Int) -> (Int, Int) -> Int
+    diagonalDistance x y = let x' = (abs $ fst x - fst y); y' = (abs $ snd x - snd y) in max x' y'
+
+    euclidianDistance :: (Int, Int) -> (Int, Int) -> Int
+    euclidianDistance x y = floor $ sqrt $ fromIntegral $ (fst x - fst y)*2 + (snd x - snd y)*2
+
+    hammingDistance :: (Int, Int) -> (Int, Int) -> Int
+    hammingDistance x y = if x == y then 1 else 0
