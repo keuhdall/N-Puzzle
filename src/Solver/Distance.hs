9da69289f8d0472673c5278782980bc24a0cf50d
Leonard Marques <keuhdall@gmail.com>
2019-04-03 03:18:04 +0200

    Update distance calculation functions signature

--- src/Solver/Distance.hs
+++ src/Solver/Distance.hs
@@ -1,9 +1,9 @@
 module Solver.Distance where
-    manhattanDistance :: (Int, Int) -> (Int, Int) -> Int
-    manhattanDistance x y = abs (fst x - fst y) + abs (snd x - snd y)
+    manhattanDistance :: (Int, Int) -> (Int, Int) -> Float
+    manhattanDistance x y = fromIntegral $ abs (fst x - fst y) + abs (snd x - snd y)
 
-    diagonalDistance :: (Int, Int) -> (Int, Int) -> Int
-    diagonalDistance x y = max $ abs (fst x - fst y) abs (snd x - snd y)
+    diagonalDistance :: (Int, Int) -> (Int, Int) -> Float
+    diagonalDistance x y = let x' = (abs $ fst x - fst y); y' = (abs $ snd x - snd y) in fromIntegral $ max x' y'
 
-    euclidianDistance :: (Int, Int) -> (Int, Int) -> Int
-    euclidianDistance x y = sqrt $ (fst x - fst y)*2 + (snd x - snd y)*2
+    euclidianDistance :: (Int, Int) -> (Int, Int) -> Float
+    euclidianDistance x y = sqrt $ fromIntegral $ (fst x - fst y)*2 + (snd x - snd y)*2
