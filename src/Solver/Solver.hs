c055a9aaae1e569e20cda81c07e7d7e384024d26
Leonard Marques <keuhdall@gmail.com>
2019-04-03 23:45:09 +0200

    Added datatype for algorithms as well as help functions

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -3,6 +3,26 @@ module Solver.Solver where
     import Solver.BTree
     import Solver.Methods
 
+    data SearchType = Astor | Uniform | Greedy deriving Eq
+
+    instance Show SearchType where
+        show Astor      = "A*"
+        show Uniform    = "Uniform cost"
+        show Greedy     = "Greedy"
+
+    getSearchType :: String -> Maybe SearchType
+    getSearchType = case s of
+        "astor"     -> Just (Astor)
+        "uniform"   -> Just (Uniform)
+        "greedy"    -> Just (Greedy)
+        _           -> Nothing
+
+    toString :: SearchType -> String
+    toString st = case st of
+        Astor   -> "astor"
+        Uniform -> "uniform"
+        Greedy  -> "greedy"
+
     getSolvedGrid :: [Int] -> [Int]
     getSolvedGrid xs = sort xs
 
