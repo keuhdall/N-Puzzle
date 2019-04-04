754c4ab99d6a1f6f85fed14a4dddb61338499da4
Leonard Marques <keuhdall@gmail.com>
2019-04-05 00:47:39 +0200

    Added utility functions for BTree module

--- src/Solver/BTree.hs
+++ src/Solver/BTree.hs
@@ -1,2 +1,14 @@
 module BTree where
     data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+    -- Utility function that create a singleton tree
+    singleton :: a -> Tree a
+    singleton x = Node x EmptyTree EmptyTree
+
+    -- Function that insert an element within the given btree
+    insert :: (Ord a) => a -> Tree a -> Tree a
+    insert x EmptyTree = singleton x
+    insert x (Node a l r)
+        | x == a    = Node x l r
+        | x < a     = Node a (insert x l) r
+        | x > a     = Node a l (insert x r)
