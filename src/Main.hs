9c310c3add65bdc3275b3b92b0aac1d90b63ed37
Léonard Marques <lmarques@student.42.fr>
2019-04-07 00:35:02 +0200

    changed imports in Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import System.Environment
+import qualified Error
+import Logger
 import Parser
 import Checker
-import Error
-import Logger
 
 main :: IO ()
 main = do
@@ -14,5 +14,5 @@ main = do
     let w = map (\l' -> words l') $ drop 1 $ clearInput l
     case transformInput w of
         Just w' -> do
-            if isSolvable (concat w') then mapM_ (\z-> putStrLn $ show z) $ concat w' else putErr NotSolvable
-        Nothing -> putErr InvalidInput
+            if isSolvable (concat w') then mapM_ (\z-> putStrLn $ show z) $ concat w' else putErr Error.NotSolvable
+        Nothing -> putErr Error.InvalidInput
