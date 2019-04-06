be81332786091a239323fcb1290d0df220fe0c9f
Léonard Marques <lmarques@student.42.fr>
2019-04-07 00:47:41 +0200

    fixed typo

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -3,23 +3,23 @@ module Solver.Solver where
     import Solver.BTree
     import Solver.Methods
 
-    data SearchType = Astor | Uniform | Greedy deriving Eq
+    data SearchType = Astar | Uniform | Greedy deriving Eq
 
     instance Show SearchType where
-        show Astor      = "A*"
+        show Astar      = "A*"
         show Uniform    = "Uniform cost"
         show Greedy     = "Greedy"
 
     getSearchType :: String -> Maybe SearchType
     getSearchType s = case s of
-        "astor"     -> Just (Astor)
+        "astar"     -> Just (Astar)
         "uniform"   -> Just (Uniform)
         "greedy"    -> Just (Greedy)
         _           -> Nothing
 
     toString :: SearchType -> String
     toString st = case st of
-        Astor   -> "astor"
+        Astar   -> "astar"
         Uniform -> "uniform"
         Greedy  -> "greedy"
 
