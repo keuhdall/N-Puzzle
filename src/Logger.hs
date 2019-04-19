619bfd2f34a1ff636dcdc84b3529503d7a4f696d
Leonard Marques <keuhdall@gmail.com>
2019-04-20 01:40:14 +0200

    updated help format

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,12 +1,14 @@
 module Logger where
     import Error
 
-    help :: IO ()
-    help = putStrLn $ "Usage : cabal run [filename] (method) (heuristic)\n \
+    displayHelp :: IO ()
+    displayHelp = putStrLn $ "Usage : cabal run [filename] (method) (heuristic)\n \
+    \ \n \
     \ Supported metods :\n \
     \ - astar \n \
     \ - uniform \n \
     \ - greedy \n \
+    \ \n \
     \ Supported heuristics : \n \
     \ - manhattan \n \ 
     \ - diagonal \n \
