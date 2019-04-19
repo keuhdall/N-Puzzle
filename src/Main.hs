fc3e5e7e9e0d270e7bb9842278b09b616974fbd0
Leonard Marques <keuhdall@gmail.com>
2019-04-20 01:40:46 +0200

    Added simple argument handling

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,25 @@
 module Main where
 
 import System.Environment
+import System.Exit
 import qualified Error as E
 import Logger
 import Parser
 import Checker
 
+checkArgs :: [String] -> IO ()
+checkArgs xs = case length xs of
+    0 -> do
+        displayHelp
+        exitSuccess
+    1 -> return () -- Solve puzzle with default algorithm and default heuristic
+    2 -> return () -- Solve puzzle with default algorithm and custom heuristic
+    _ -> return () -- Solve puzzle with custom algorithm and custom heuristic
+
 main :: IO ()
 main = do
-    args <- getArgs 
+    args <- getArgs
+    checkArgs args
     content <- readFile (args !! 0)
     let l = lines content
     let w = map words $ drop 1 $ clearInput l
