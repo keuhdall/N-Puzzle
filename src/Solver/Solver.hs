5bbcc4faa8ba06cf46f2b1310599b04f5a1d60da
Leonard Marques <keuhdall@gmail.com>
2019-04-21 00:40:55 +0200

    Moved getIndexes function

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -1,5 +1,11 @@
 module Solver.Solver where
     import Data.Sort
+    import Solver.Grid
     import Solver.Methods
+    import Solver.Distance
+
+    getNextNodes :: [Int] -> Distance -> [(Int, Int)]
+    getNextNodes xs d = let size = getPuzzleSize xs; ys = getSolvedGrid size; xs' = getNeighbors xs; dist = getDistance d in
+        map (\x -> ((fromCoordinates xs x), (dist (getCoordinates ys $ fromCoordinates xs x) x))) xs'
 
     --solve :: [Int] -> (Tree -> ((Int, Int) -> (Int, Int) -> Int) -> [Int] -> [Int] -> Tree) -> Tree
