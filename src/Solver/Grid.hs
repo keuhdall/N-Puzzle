9862b763edd8a2dd8f6020bbf6b1e64d5505feb4
Leonard Marques <keuhdall@gmail.com>
2019-04-22 03:46:51 +0200

    cleaned getSolvedGrid function

--- src/Solver/Grid.hs
+++ src/Solver/Grid.hs
@@ -3,13 +3,11 @@ module Solver.Grid where
     -- Returns a solved grid of the given size
     getSolvedGrid :: Int -> [Int]
     getSolvedGrid n = let xs = replicate (n^2) (-1) in getSolvedGrid' xs 1 0 1 0 0 where
-        getSolvedGrid' :: [Int] -> Int -> Int -> Int -> Int -> Int -> [Int]
         getSolvedGrid' xs' cur x ix y iy
             | cur == n^2 = replace xs' 0 (x+y*n)
-            | (x + ix == n) || (x + ix < 0) || (ix /= 0 && (xs' !! (x+ix+y*n)) /= (-1)) = getSolvedGrid' (replace xs' cur $ x+y*n) (cur+1) x 0 (y+ix) ix
-            | (y + iy == n) || (y + iy < 0) || (iy /= 0 && (xs' !! (x+(y+iy)*n)) /= (-1)) = getSolvedGrid' (replace xs' cur $ x+y*n) (cur+1) (x-iy) (-iy) y 0
+            | (x + ix == n) || (x + ix < 0) || (ix /= 0 && (xs' !! (x+ix+y*n)) /= (-1))     = getSolvedGrid' (replace xs' cur $ x+y*n) (cur+1) x 0 (y+ix) ix
+            | (y + iy == n) || (y + iy < 0) || (iy /= 0 && (xs' !! (x+(y+iy)*n)) /= (-1))   = getSolvedGrid' (replace xs' cur $ x+y*n) (cur+1) (x-iy) (-iy) y 0
             | otherwise = getSolvedGrid' (replace xs' cur $ x+y*n) (cur+1) (x+ix) ix (y+iy) iy
-        replace :: [a] -> a -> Int -> [a]
         replace [] _ _ = []
         replace (x:xs) x' n
             | n == 0    = x':xs
