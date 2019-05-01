8acd723f73590181642154681b522b0e816eb6ce
Leonard Marques <keuhdall@gmail.com>
2019-05-02 00:59:28 +0200

    A* seems to be working, but is very slow

--- src/Solver/Methods.hs
+++ src/Solver/Methods.hs
@@ -6,7 +6,7 @@ module Solver.Methods where
     import Solver.Grid
 
     data SearchType = Astar | Uniform | Greedy deriving Eq
-    type NextNodesFunc = [Int] -> Distance -> PQ.MaxPQueue Int Int
+    type NextNodesFunc = [Int] -> Distance -> PQ.MaxPQueue Int Int -> PQ.MaxPQueue Int Int
 
     instance Show SearchType where
         show Astar      = "A*"
@@ -18,11 +18,13 @@ module Solver.Methods where
         readsPrec _ "uniform"   = [(Uniform, "uniform")]
         readsPrec _ "greedy"    = [(Greedy, "greedy")]
 
-    astar :: [Int] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> NextNodesFunc -> IO ()
-    astar xs os cs d nn
-        | xs == getSolvedGrid (getPuzzleSize xs) = displayGrid xs
-        | otherwise = do
-            let cln = PQ.filter (\x -> (swapValues x 0 xs) `notElem` cs) os
-            if (cln == PQ.empty) then putErr E.NotSolvable else displayGrid xs >> (astar (swp cln) nxt (xs:cs) d nn) where
-                swp a = swapValues (snd $ PQ.findMax a) 0 xs
-                nxt = nn (swp os) d
+    astar :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> NextNodesFunc -> Int -> IO ()
+    astar xss os cs d nn n
+        | xs == getSolvedGrid (getPuzzleSize xs) = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")
+        | cln == PQ.empty && os /= PQ.empty = astar (tail xss) os (xs:cs) d nn (n+1)
+        | otherwise = if (cln == PQ.empty) then putErr E.NotSolvable else displayGrid xs >> (astar ((swp cln):xss) nxt (xs:cs) d nn) (n+1) where
+            xs = head xss
+            swp x = swapValues (snd $ PQ.findMax x) 0 xs
+            gn xs = map (fromCoordinates xs) $ getNeighbors xs
+            nxt = nn (swp os) d os
+            cln = PQ.filter (\x -> ((swapValues x 0 xs) `notElem` cs) && (x `elem` (gn xs))) os
--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -5,12 +5,12 @@ module Solver.Solver where
     import Solver.Methods
     import Solver.Distance
 
-    getNextNodes :: [Int] -> Distance -> PQ.MaxPQueue Int Int
-    getNextNodes xs d = getNextNodes' (getNeighbors xs) PQ.empty where
+    getNextNodes :: [Int] -> Distance -> PQ.MaxPQueue Int Int -> PQ.MaxPQueue Int Int
+    getNextNodes xs d pq = getNextNodes' (getNeighbors xs) pq where
         svd = getSolvedGrid $ getPuzzleSize xs
         dist = getDistance d
         getNextNodes' (y:[]) pq = PQ.insert (dist (getCoordinates svd $ fromCoordinates xs y) y) (fromCoordinates xs y) pq
         getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (dist (getCoordinates svd $ fromCoordinates xs y) y) (fromCoordinates xs y) pq)
 
     solve :: [Int] -> SearchType -> Distance -> IO ()
-    solve xs st d = astar xs (getNextNodes xs d) [] d getNextNodes
+    solve xs st d = astar [xs] (getNextNodes xs d PQ.empty) [] d getNextNodes 0
