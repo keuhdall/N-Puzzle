f7d746a258a3f6716ad5cea3c8eb5091df3abda7
keuhdall <lmarques@student.42.fr>
2019-05-07 18:07:21 +0200

    updated node cost calculation ; Uniform cost added

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -8,6 +8,7 @@ module Solver.Solver (SearchType(..), getSearchType, solve) where
 
     data SearchType = Astar | Uniform | Greedy deriving Eq
     type SeachFunc = [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> Int -> IO ()
+    type DistFunc = (Int, Int) -> (Int, Int) -> Int
 
     instance Show SearchType where
         show Astar      = "A*"
@@ -24,12 +25,24 @@ module Solver.Solver (SearchType(..), getSearchType, solve) where
         Astar   -> astar
         Greedy  -> greedy
 
-    getNextNodes :: [Int] -> Int -> Distance -> PQ.MaxPQueue Int Int -> PQ.MaxPQueue Int Int
-    getNextNodes xs psize d pq = getNextNodes' (getNeighbors xs) pq where
-        svd = getSolvedGrid $ getPuzzleSize xs
-        dist = getDistance d
-        getNextNodes' (y:[]) pq = PQ.insert (psize + (dist (getCoordinates svd $ fromCoordinates xs y) y)) (fromCoordinates xs y) pq
-        getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (psize + (dist (getCoordinates svd $ fromCoordinates xs y) y)) (fromCoordinates xs y) pq)
+    getCost :: SearchType -> DistFunc -> [[Int]] -> (Int, Int) -> Int
+    getCost st d xss coord =
+        let xs    =  head xss
+            svd   =  getSolvedGrid $ getPuzzleSize xs
+            val   =  fromCoordinates xs coord
+            dist  =  d (getCoordinates svd val) coord in case st of
+            Astar     -> dist + (length xss)
+            Uniform   -> (length xss)
+            Greedy    -> dist
+
+    getNextNodes :: [[Int]] -> [Int] -> SearchType -> Distance -> PQ.MaxPQueue Int Int -> PQ.MaxPQueue Int Int
+    getNextNodes xss xs st d pq = getNextNodes' (getNeighbors xs) pq where
+        svd   = getSolvedGrid $ getPuzzleSize xs
+        dist  = getDistance d
+        cost  = getCost st dist xss
+        value = fromCoordinates xs
+        getNextNodes' (y:[]) pq = PQ.insert (cost y) (value y) pq
+        getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (cost y) (value y) pq)
 
     astar :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> Int -> IO ()
     astar xss os cs d n
@@ -41,7 +54,7 @@ module Solver.Solver (SearchType(..), getSearchType, solve) where
             swp x   =  swapValues (snd $ PQ.findMax x) 0 xs
             gn xs   =  map (fromCoordinates xs) $ getNeighbors xs
             cln     =  PQ.filter (\x -> ((swapValues x 0 xs) `notElem` cs) && (x `elem` (gn xs))) os
-            nxt     =  getNextNodes (swp cln) (length xss) d os
+            nxt     =  getNextNodes xss (swp cln) Astar d os
 
     greedy :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> Int -> IO ()
     greedy xss os cs d n
@@ -53,7 +66,20 @@ module Solver.Solver (SearchType(..), getSearchType, solve) where
             swp x   =  swapValues (snd $ PQ.findMax x) 0 xs
             gn xs   =  map (fromCoordinates xs) $ getNeighbors xs
             cln     =  PQ.filter (\x -> ((swapValues x 0 xs) `notElem` cs) && (x `elem` (gn xs))) os
-            nxt     =  getNextNodes (swp cln) 0 d os
+            nxt     =  getNextNodes xss (swp cln) Greedy d os
+
+    uniform :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> Distance -> Int -> IO ()
+    uniform xss os cs d n
+        | xs == getSolvedGrid (getPuzzleSize xs) = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")
+        | cln == PQ.empty && os /= PQ.empty = greedy (tail xss) os (xs:cs) d (n+1)
+        | cln == PQ.empty = putErr E.NotSolvable
+        | otherwise = displayGrid xs >> (greedy ((swp cln):xss) nxt (xs:cs) d (n+1)) where
+            xs      =  head xss
+            swp x   =  swapValues (snd $ PQ.findMax x) 0 xs
+            gn xs   =  map (fromCoordinates xs) $ getNeighbors xs
+            cln     =  PQ.filter (\x -> ((swapValues x 0 xs) `notElem` cs) && (x `elem` (gn xs))) os
+            nxt     =  getNextNodes xss (swp cln) Uniform d os
+        
 
     solve :: [Int] -> SearchType -> Distance -> IO ()
-    solve xs st d = astar [xs] (getNextNodes xs 0 d PQ.empty) [] d 0
+    solve xs st d = astar [xs] (getNextNodes [xs] xs st d PQ.empty) [] d 0
