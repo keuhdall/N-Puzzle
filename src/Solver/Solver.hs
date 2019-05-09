ccdb1504245dc1d675adee71ea89e725a8a29357
keuhdall <lmarques@student.42.fr>
2019-05-09 21:22:44 +0200

    added some comments to the search function

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -37,17 +37,18 @@ module Solver.Solver (SearchType(..), solve) where
         getNextNodes' (y:[]) pq = PQ.filter (\x -> (swapValues x xs) `notElem` cs) $ PQ.insert (cost y) (value y) pq
         getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (cost y) (value y) pq)
 
+    -- Runs the search using a given SearchType. The SearchType will be used in nodes cost computation
     runSearch :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> SearchType -> NextNodesFunc -> Int -> IO ()
     runSearch xss os cs st nn n
         | xs == getSolvedGrid (getPuzzleSize xs) = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")
         | cln == PQ.empty && os /= PQ.empty = displayGrid xs >> runSearch (tail xss) os (xs:cs) st nn (n+1)
         | cln == PQ.empty = putErr E.NotSolvable
         | otherwise = displayGrid xs >> runSearch ((swapValues (snd max) xs):xss) os' (xs:cs) st nn (n+1) where
-                xs  = head xss
-                gn  = map (fromCoordinates xs) $ getNeighbors xs
-                cln = PQ.filter ((flip elem) gn) $ PQ.union (nn st xss cs) os
-                max = PQ.findMax cln
-                os' = PQ.filterWithKey (\x y -> (x /= (fst max) || y /= (snd max))) $ PQ.union (nn st xss cs) os
+                xs  = head xss                                                                                      -- Current state of the board
+                gn  = map (fromCoordinates xs) $ getNeighbors xs                                                    -- Neighbors as [Int]
+                cln = PQ.filter ((flip elem) gn) $ PQ.union (nn st xss cs) os                                       -- Neighbors drawed from the PQueue
+                max = PQ.findMax cln                                                                                -- Maximum value retrieved from the neighbors drawed from the PQueue
+                os' = PQ.filterWithKey (\x y -> (x /= (fst max) || y /= (snd max))) $ PQ.union (nn st xss cs) os    -- New open set
 
     solve :: [Int] -> SearchType -> Distance -> IO ()
     solve xs st d = let nn = getNextNodes d in runSearch [xs] PQ.empty [] st nn 0
