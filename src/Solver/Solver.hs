29c3ae89629ece8b26c4ae3d5cf8f46ae13f7ceb
keuhdall <lmarques@student.42.fr>
2019-05-11 20:14:22 +0200

    changed type of PQueue ; fixing search function

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -8,7 +8,7 @@ module Solver.Solver (SearchType(..), readSearchType, solve) where
 
     data SearchType = Astar | Uniform | Greedy deriving Eq
     type DistFunc = (Int, Int) -> (Int, Int) -> Int
-    type NextNodesFunc = SearchType -> [[Int]] -> [[Int]] -> PQ.MaxPQueue Int Int
+    type NextNodesFunc = SearchType -> [[Int]] -> [[Int]] -> PQ.MaxPQueue Int [[Int]]
 
     instance Show SearchType where
         show Astar      = "A*"
@@ -22,7 +22,6 @@ module Solver.Solver (SearchType(..), readSearchType, solve) where
         "greedy"    -> Just Greedy
         _           -> Nothing
 
-
     -- Returns the cost of a node according to the SearchType currently used
     getCost :: SearchType -> DistFunc -> [[Int]] -> (Int, Int) -> Int
     getCost st d xss coord =
@@ -35,29 +34,29 @@ module Solver.Solver (SearchType(..), readSearchType, solve) where
             Greedy    -> dist                   -- Greedy : h cost only
 
     -- Returns a PQueue containing the next nodes (value + cost)
-    getNextNodes :: Distance -> SearchType -> [[Int]] -> [[Int]] -> PQ.MaxPQueue Int Int
+    getNextNodes :: Distance -> SearchType -> [[Int]] -> [[Int]] -> PQ.MaxPQueue Int [[Int]]
     getNextNodes d st xss cs = getNextNodes' (getNeighbors xs) PQ.empty where
         xs    = head xss
         cost  = getCost st (getDistance d) xss
-        value = fromCoordinates xs
-        getNextNodes' (y:[]) pq = PQ.filter (\x -> (swapValues x xs) `notElem` cs) $ PQ.insert (cost y) (value y) pq
+        value x = (swapValues (fromCoordinates xs x) xs):xss
+        getNextNodes' (y:[]) pq = PQ.filter ((flip $ notElem . head) cs) $ PQ.insert (cost y) (value y) pq
         getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (cost y) (value y) pq)
 
     -- Runs the search using a given SearchType. The SearchType will be used in nodes cost computation
-    runSearch :: [[Int]] -> PQ.MaxPQueue Int Int -> [[Int]] -> SearchType -> NextNodesFunc -> Int -> IO ()
+    runSearch :: [[Int]] -> PQ.MaxPQueue Int [[Int]] -> [[Int]] -> SearchType -> NextNodesFunc -> Int -> IO ()
     runSearch xss os cs st nn n
-        | isSolved xs = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")                      -- Puzzle is solved
-        | cln == PQ.empty && os /= PQ.empty = displayGrid xs >> runSearch (tail xss) os (xs:cs) st nn (n+1)     -- All neighbors leads to grids in the close set : we backtrack to 
-        | cln == PQ.empty && os == PQ.empty = putErr E.NotSolvable                                              -- Open set is empty, all solutions have been exhauted : the puzzle is not solvable
-        | otherwise = displayGrid xs >> runSearch ((swapValues (snd max) xs):xss) os' (xs:cs) st nn (n+1) where -- Search keeps going with closest neighbor
-            xs  = head xss                                                                                   -- Current state of the board
-            gn  = map (fromCoordinates xs) $ getNeighbors xs                                                 -- Neighbors as [Int]
-            cln = PQ.filter ((flip elem) gn) $ PQ.union (nn st xss cs) os                                    -- Neighbors drawed from the PQueue
-            max = PQ.findMax cln                                                                             -- Maximum value retrieved from the neighbors drawed from the PQueue
-            os' = PQ.filterWithKey (\x y -> (x /= (fst max) || y /= (snd max))) $ PQ.union (nn st xss cs) os -- New open set
+        | isSolved xs = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")
+        | suc /= PQ.empty                                       = displayGrid xs >> runSearch ((max suc):xss)   (PQ.union os $ PQ.deleteMax suc)                (xs:cs) st nn (n+1)
+        | suc == PQ.empty && os' /= PQ.empty                    = displayGrid xs >> runSearch ((max os'):xss)   (PQ.filter (/= (snd . PQ.findMax $ os')) os)    (xs:cs) st nn (n+1)
+        | suc == PQ.empty && os' == PQ.empty && os /= PQ.empty  = displayGrid xs >> runSearch (tail xss)        os                                              (xs:cs) st nn (n+1)
+        | otherwise = putErr E.NotSolvable where
+            xs      = head xss
+            suc     = nn st xss cs
+            os'     = PQ.filter (\x -> tail x == tail xss) os
+            max x   = head . snd $ PQ.findMax x
 
     solve :: [Int] -> (Maybe SearchType, Maybe Distance) -> IO ()
-    solve xs (Nothing, Nothing) = putStrLn ("Solving grid using the Astar algorihtm and the Manhattan distance") >> runSearch [xs] PQ.empty [] Astar (getNextNodes Manhattan) 0
-    solve xs (Just st, Nothing) = putStrLn ("Solving grid using the " ++ (show st) ++ " algorihtm and the Manhattan distance") >> runSearch [xs] PQ.empty [] st (getNextNodes Manhattan) 0
-    solve xs (Nothing, Just d)  = putStrLn ("Solving grid using the Astar algorihtm and the " ++ (show d) ++ " distance") >> runSearch [xs] PQ.empty [] Astar (getNextNodes d) 0
-    solve xs (Just st, Just d)  = putStrLn ("Solving grid using the " ++ (show st) ++ " algorihtm and the " ++ (show d) ++ " distance") >> runSearch [xs] PQ.empty [] st (getNextNodes d) 0
+    solve xs (Nothing, Nothing) = putStrLn ("Solving grid using the Astar algorihtm and the Manhattan distance")                        >> runSearch  [xs]  PQ.empty  []  Astar  (getNextNodes Manhattan)  0
+    solve xs (Just st, Nothing) = putStrLn ("Solving grid using the " ++ (show st) ++ " algorihtm and the Manhattan distance")          >> runSearch  [xs]  PQ.empty  []  st     (getNextNodes Manhattan)  0
+    solve xs (Nothing, Just d)  = putStrLn ("Solving grid using the Astar algorihtm and the " ++ (show d) ++ " distance")               >> runSearch  [xs]  PQ.empty  []  Astar  (getNextNodes d)          0
+    solve xs (Just st, Just d)  = putStrLn ("Solving grid using the " ++ (show st) ++ " algorihtm and the " ++ (show d) ++ " distance") >> runSearch  [xs]  PQ.empty  []  st     (getNextNodes d)          0
