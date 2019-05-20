19f229bf9bde9f8701a0a514bec8fd6b6fd713c8
keuhdall <lmarques@student.42.fr>
2019-05-20 22:05:31 +0200

    started updating Solver module with new data structure

--- src/Solver/Solver.hs
+++ src/Solver/Solver.hs
@@ -1,6 +1,6 @@
 module Solver.Solver (SearchType(..), readSearchType, solve) where
     import Data.Sort
-    import qualified Data.PQueue.Prio.Max as PQ
+    import qualified Data.PQueue.Prio.Min as PQ
     import qualified Data.HashSet as S
     import qualified Error as E
     import Logger
@@ -8,8 +8,7 @@ module Solver.Solver (SearchType(..), readSearchType, solve) where
     import Solver.Distance
 
     data SearchType = Astar | Uniform | Greedy deriving Eq
-    type DistFunc = (Int, Int) -> (Int, Int) -> Int
-    type NextNodesFunc = [Grid] -> S.Set Grid -> PQ.MaxPQueue Int [Grid]
+    type NextNodesFunc = [Grid] -> S.Set Grid -> PQ.MinPQueue Int [Grid]
 
     instance Show SearchType where
         show Astar      = "A*"
@@ -24,37 +23,28 @@ module Solver.Solver (SearchType(..), readSearchType, solve) where
         _           -> Nothing
 
     -- Returns the cost of a node according to the SearchType currently used
-    getCost :: SearchType -> DistFunc -> [Grid] -> (Int, Int) -> Int
-    getCost st d xss coord =
-        let xs    =  head xss
-            svd   =  getSolvedGrid $ getPuzzleSize xs
-            val   =  fromCoordinates xs coord
-            dist  =  d coord (getCoordinates svd val) in case st of
+    getCost :: SearchType -> Distance -> [Grid] -> Grid -> Grid -> Int
+    getCost st d xss grid goal = let dist = calcDistance grid goal in case st of
             Astar     -> dist + (length xss)    -- A* : h cost + g cost
             Uniform   -> (length xss)           -- Uniform : g cost only
             Greedy    -> dist                   -- Greedy : h cost only
 
     -- Returns a PQueue containing the next nodes (value + cost)
-    getNextNodes :: Distance -> SearchType -> [Grid] -> S.Set Grid -> PQ.MaxPQueue Int [Grid]
-    getNextNodes d st xss cs = getNextNodes' (getNeighbors xs) PQ.empty where
-        xs    = head xss
-        cost  = getCost st (getDistance d) xss
-        value x = (swapValues (fromCoordinates xs x) xs):xss
-        getNextNodes' (y:[]) pq = PQ.filter ((flip $ S.notMember . head) cs) $ PQ.insert (cost y) (value y) pq
-        getNextNodes' (y:ys) pq = getNextNodes' ys (PQ.insert (cost y) (value y) pq)
+    getNextNodes :: Distance -> SearchType -> [Grid] -> Grid -> PQ.MinPQueue Int [Grid]
+    getNextNodes d st xss goal = map (getCost st d xss goal) $ getNeighbors $ head xss
 
     -- Runs the search using a given SearchType. The SearchType will be used in nodes cost computation
-    runSearch :: [Grid] -> PQ.MaxPQueue Int [Grid] -> S.Set Grid -> NextNodesFunc -> Int -> IO ()
+    runSearch :: [Grid] -> PQ.MinPQueue Int [Grid] -> S.Set Grid -> NextNodesFunc -> Int -> IO ()
     runSearch xss os cs nn n
         | isSolved xs = displayGrid xs >> putStrLn ("Solved in " ++ (show n) ++ " steps.")
-        | suc /= PQ.empty                                       = runSearch ((max suc):xss)   (PQ.union os $ PQ.deleteMax suc)                cs' nn (n+1)
-        | suc == PQ.empty && os' /= PQ.empty                    = runSearch ((max os'):xss)   (PQ.filter (/= (snd . PQ.findMax $ os')) os)    cs' nn (n+1)
+        | suc /= PQ.empty                                       = runSearch ((max suc):xss)   (PQ.union os $ PQ.deleteMin suc)                cs' nn (n+1)
+        | suc == PQ.empty && os' /= PQ.empty                    = runSearch ((max os'):xss)   (PQ.filter (/= (snd . PQ.findMin $ os')) os)    cs' nn (n+1)
         | suc == PQ.empty && os' == PQ.empty && os /= PQ.empty  = runSearch (tail xss)        os                                              cs' nn (n+1)
         | otherwise = putErr E.NotSolvable where
             xs      = head xss
             suc     = nn xss cs
             os'     = PQ.filter (\x -> tail x == tail xss) os
-            max x   = head . snd $ PQ.findMax x
+            max x   = head . snd $ PQ.findMin x
             cs'     = S.insert xs cs
 
     solve :: Grid -> (Maybe SearchType, Maybe Distance) -> IO ()
