5ac7547877906bfa0c5a5bc01e77857d005301a4
justasze <justasze@student.42.fr>
2019-06-11 17:29:39 +0200

    Removing typos and adding a 5x5 grid.

--- src/Error.hs
+++ src/Error.hs
@@ -2,6 +2,6 @@ module Error where
     data Error = NotSolvable | InvalidInput | InvalidSearchType
 
     instance Show Error where
-        show NotSolvable        = "Error : puzzle is not solvable."
-        show InvalidInput       = "Error : input is invalid."
-        show InvalidSearchType  = "Error : invalid search type."
+        show NotSolvable        = "Error: puzzle is not solvable."
+        show InvalidInput       = "Error: input is invalid."
+        show InvalidSearchType  = "Error: invalid search type."
